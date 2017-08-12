f0f1ccd58c0c2e814c53f049c1d9aabeabb2d4b9
Raphael Rodrigues <raphael.rodrigues@dexatec.com.br>
2017-08-11 21:54:56 -0400

    Added Moment.js library and created the first page with a functional local-time clock

--- world-time-manager/src/pages/home/home.ts
+++ world-time-manager/src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  time;
 
   constructor(public navCtrl: NavController) {
-
+    //this.time = Date.now();
+    setInterval(() => {
+      this.time = moment().format('LTS');
+    }, 1000)
   }
 
 }
