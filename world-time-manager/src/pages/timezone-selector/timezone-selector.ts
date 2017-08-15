705be9b5fe4c71f2efb58eb20ef0a2b1daa0ae6b
Raphael Rodrigues <raphael.rodrigues@dexatec.com.br>
2017-08-15 02:48:45 -0400

    Created a Searchbar that filters the list of timezones making it easier to find the desired TZ

--- world-time-manager/src/pages/timezone-selector/timezone-selector.ts
+++ world-time-manager/src/pages/timezone-selector/timezone-selector.ts
@@ -8,17 +8,33 @@ import * as moment from 'moment-timezone';
   templateUrl: 'timezone-selector.html',
 })
 export class TimezoneSelectorPage {
-  zones = moment.tz.names();
+  zones;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public event: Events
-  ){}
+  ){
+    this.refreshList();
+  }
 
   selectClock(zone) {
     this.event.publish('createNewClock', zone);
     this.navCtrl.pop();
+  }
+
+  refreshList() {
+    this.zones = moment.tz.names();
+  }
+
+  filterList(ev) {
+    this.refreshList();
+    let val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.zones = this.zones.filter((zone) => {
+        return (zone.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    };
   }
 
 }
